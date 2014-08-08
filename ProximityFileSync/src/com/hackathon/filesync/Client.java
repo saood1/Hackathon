6875b7d7c95a7f98b5847941e6baca6d2071e26c
Mohamed Saood Khan <mohamed.khan@salesforce.com>
2014-08-08 21:24:41 +0530

    Base version.

---
+++ ProximityFileSync/src/com/hackathon/filesync/Client.java
@@ -0,0 +1,9 @@
+package com.hackathon.filesync;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ ProximityFileSync/src/com/hackathon/filesync/Server.java
@@ -0,0 +1,9 @@
+package com.hackathon.filesync;
+
+public class Server {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
