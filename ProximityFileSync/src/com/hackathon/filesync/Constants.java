784f3a2dd115c7b45c22980bbeabbe9f7b6414be
rajuranjan <raju.ranjan@rranjan1-ltm.internal.salesforce.com>
2014-08-21 19:19:28 +0530

    Adding comments to the constant file

--- ProximityFileSync/src/com/hackathon/filesync/Constants.java
+++ ProximityFileSync/src/com/hackathon/filesync/Constants.java
@@ -1,13 +1,17 @@
 package com.hackathon.filesync;
 
 public class Constants {
+	
+	//Server Details
 	public static final int SERVER_PORT_NO                  = 65535;
 	public static final String SERVER_IP_ADDRESS            = "127.0.0.1";
 	
+	//Strings for JSON Keys
 	public static final String PORT_NO                      = "PORTNO";
 	public static final String IP_ADDRESS                   = "IP_ADDRESS";
 	public static final String CORDINATES                   = "CORDINATES";
 	
+	//JSON Task keys
 	public static final String CLIENT_INFORMATION           = "CLIENT_INFORMATION";
 	public static final String TASK_INFORMATION             = "TASK_INFORMATION";
 }
