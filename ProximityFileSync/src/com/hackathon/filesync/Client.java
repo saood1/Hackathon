d457bdbaf01b1a5cbcc02b24fc45279433503009
Raju Ranjan <raju.ranjan@salesforce.com>
2014-08-22 01:37:16 +0530

    Adding client class

---
+++ ProximityFileSync/src/com/hackathon/filesync/Client.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+public class Client {
+	private GeoLocation m_location;
+
+	private String m_ip;
+	private Integer m_port;
+
+	public Client(GeoLocation location,  String ip, Integer port) {
+		super();
+		this.m_location = location;
+		this.m_ip = ip;
+		this.m_port = port;
+	}
+
+	public GeoLocation getLocation() {
+		return m_location;
+	}
+
+	public void setLocation(GeoLocation location) {
+		this.m_location = location;
+	}
+
+	public String getIp() {
+		return m_ip;
+	}
+
+	public void setIp(String ip) {
+		this.m_ip = ip;
+	}
+
+	public int getPort() {
+		return m_port;
+	}
+
+	public void setPort(Integer port) {
+		this.m_port = port;
+	}
+
+}
